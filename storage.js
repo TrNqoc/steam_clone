086665207434ff34cb0622b6ea9e3258b017a485
TrNqoc <truongngocdth@gmail.com>
2025-11-29 08:33:40 +0700

    fix login

--- storage.js
+++ storage.js
@@ -1,4 +1,29 @@
 const WISHLIST_KEY = "wishlist";
+// Trả về danh sách user đã đăng ký
+export function getUsers() {
+  return JSON.parse(localStorage.getItem("users")) || [];
+}
+
+// Lưu danh sách user
+export function saveUsers(list) {
+  localStorage.setItem("users", JSON.stringify(list));
+}
+
+// Ghi lại user đang đăng nhập
+export function setCurrentUser(username) {
+  localStorage.setItem("currentUser", username);
+}
+
+// Lấy user đang đăng nhập
+export function getCurrentUser() {
+  return localStorage.getItem("currentUser");
+}
+
+// Logout
+export function logoutUser() {
+  localStorage.removeItem("currentUser");
+}
+
 function getWishlist() {
   return JSON.parse(localStorage.getItem("wishlist")) || [];
 }
